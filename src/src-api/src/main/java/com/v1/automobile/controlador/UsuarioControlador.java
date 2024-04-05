89bc5e0c748a108478bad3227d9e617ecae17941
ArtashesAA <116360537+ArtashesAA@users.noreply.github.com>
2024-04-05 22:48:17 +0200

    + Controlador Usuario

--- src/src-api/src/main/java/com/v1/automobile/controlador/UsuarioControlador.java
+++ src/src-api/src/main/java/com/v1/automobile/controlador/UsuarioControlador.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,24 +15,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.v1.automobile.entidad.Usuario;
-import com.v1.automobile.servicio.UsuarioServicio;
+import com.v1.automobile.repositorio.UsuarioRepositorio;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
 public class UsuarioControlador {
 
 	@Autowired
-	private UsuarioServicio usuarioServicio;
+	private UsuarioRepositorio usuarioRepositorio;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@GetMapping
 	public ResponseEntity<List<Usuario>> obtenerTodosUsuarios() {
-		List<Usuario> usuarios = usuarioServicio.obtenerUsuarios();
+		List<Usuario> usuarios = usuarioRepositorio.findAll();
 		return new ResponseEntity<>(usuarios, HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Integer id) {
-		Usuario usuario = usuarioServicio.obtenerUsuarioPorId(id);
+		Usuario usuario = usuarioRepositorio.findById(id).orElse(null);
 		if (usuario != null) {
 			return new ResponseEntity<>(usuario, HttpStatus.OK);
 		} else {
@@ -41,15 +45,26 @@ public class UsuarioControlador {
 
 	@PostMapping
 	public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario) {
-		Usuario usuarioCreado = usuarioServicio.guardarUsuario(usuario);
+		Usuario usuarioCreado = usuario;
+		usuarioCreado.setPassword(passwordEncoder.encode(usuario.getPassword()));
+		usuarioRepositorio.save(usuarioCreado);
 		return new ResponseEntity<>(usuarioCreado, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<Usuario> actualizarUsuario(@PathVariable Integer id,
 			@RequestBody Usuario usuarioActualizado) {
-		usuarioServicio.actualizarUsuario(id, usuarioActualizado);
-		if (usuarioActualizado != null) {
+		Usuario usuarioExistente = usuarioRepositorio.findById(id).orElse(null);
+		if (usuarioExistente != null) {
+			usuarioExistente.setNombre_usuario(usuarioActualizado.getNombre_usuario());
+			usuarioExistente.setEmail(usuarioActualizado.getEmail());
+			usuarioExistente.setImagen_usuario(usuarioActualizado.getImagen_usuario());
+
+			if (!usuarioActualizado.getPassword().isEmpty()) {
+				usuarioExistente.setPassword(passwordEncoder.encode(usuarioActualizado.getPassword()));
+			}
+			usuarioExistente.setRole(usuarioActualizado.getRole());
+			usuarioRepositorio.save(usuarioExistente);
 			return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -58,10 +73,10 @@ public class UsuarioControlador {
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> eliminarUsuario(@PathVariable Integer id) {
-		if (!usuarioServicio.existe(id)) {
+		if (!usuarioRepositorio.existsById(id)) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		usuarioServicio.borrarUsuario(id);
+		usuarioRepositorio.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
--- src/src-api/src/main/java/com/v1/automobile/servicio/UsuarioServicio.java
+++ src/src-api/src/main/java/com/v1/automobile/servicio/UsuarioServicio.java
@@ -1,15 +1,11 @@
 package com.v1.automobile.servicio;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.v1.automobile.entidad.Usuario;
 import com.v1.automobile.repositorio.UsuarioRepositorio;
 
 @Service
@@ -18,54 +14,9 @@ public class UsuarioServicio implements UserDetailsService {
 	@Autowired
 	private UsuarioRepositorio usuarioRepositorio;
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return usuarioRepositorio.findByEmail(username).orElseThrow();
-	}
-
-	public Usuario guardarUsuario(Usuario usuario) {
-		// Cifrar la contraseña antes de guardarla
-		usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
-		return usuarioRepositorio.save(usuario);
-	}
-
-	public List<Usuario> obtenerUsuarios() {
-		return usuarioRepositorio.findAll();
-	}
-
-	public Usuario obtenerUsuarioPorId(Integer id) {
-		return usuarioRepositorio.findById(id).orElse(null);
-	}
-
-	public Usuario actualizarUsuario(Integer id, Usuario usuarioActualizado) {
-		Usuario usuarioExistente = usuarioRepositorio.findById(id).orElse(null);
-		if (usuarioExistente != null) {
-			usuarioExistente.setNombre_usuario(usuarioActualizado.getNombre_usuario());
-			usuarioExistente.setEmail(usuarioActualizado.getEmail());
-			usuarioExistente.setImagen_usuario(usuarioActualizado.getImagen_usuario());
-
-			if (!usuarioActualizado.getPassword().isEmpty()) {
-				usuarioExistente.setPassword(passwordEncoder.encode(usuarioActualizado.getPassword()));
-			}
-			usuarioExistente.setRole(usuarioActualizado.getRole());
-			return usuarioRepositorio.save(usuarioExistente);
-		}
-		return null;
-	}
-
-	public boolean borrarUsuario(Integer id) {
-		if (usuarioRepositorio.existsById(id)) {
-			usuarioRepositorio.deleteById(id);
-			return true;
-		}
-		return false;
-	}
-
-	public boolean existe(Integer id) {
-		return usuarioRepositorio.existsById(id);
 	}
 
 }
